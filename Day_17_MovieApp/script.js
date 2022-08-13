0c58d25801534a335bad543dc13cb6ee9ee6baca
Filip Kocian <filkocian@gmail.com>
2022-08-13 17:32:23 +0200

    Day 17 solution

---
+++ Day_17_MovieApp/script.js
@@ -0,0 +1,62 @@
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=afe4f6d2da64b981770a045a21301033&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=afe4f6d2da64b981770a045a21301033&page=1&query="';
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const main = document.getElementById("main");
+
+// Get initial movies
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  showMovies(data.results);
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
+
+function showMovies(movies) {
+  main.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = `    
+    <div class="movie">
+        <img src="${IMG_PATH + poster_path}" alt="${title}"/>
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h3>Overview</h3>
+            ${overview}
+        </div>
+    </div>`;
+
+    main.insertAdjacentHTML("beforeend", movieEl);
+  });
+}
+
+function getClassByRate(rate) {
+  if (rate >= 8) return "green";
+  else if (rate >= 5) return "orange";
+  else return "red";
+}
