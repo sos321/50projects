dfbe70387e345b0764b98ce72a6fb058cea5f1b8
Filip Kocian <filkocian@gmail.com>
2022-08-27 22:35:22 +0200

    Day 21 solution

---
+++ Day_21_DragnDrop/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+
+empties.forEach((empty) => {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+});
+
+function dragStart() {
+  this.className += " hold";
+  setTimeout(() => (this.className = "invisible"), 1);
+}
+
+function dragEnd() {
+  this.className = "fill";
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+
+  this.className += " hovered";
+}
+
+function dragLeave() {
+  this.className = "empty";
+}
+
+function dragDrop(e) {
+  e.preventDefault();
+
+  this.className = "empty";
+  this.append(fill);
+}
