235b7447cff1ac0131433d52046efc376d7b37e3
Filip Kocian <filkocian@gmail.com>
2022-10-30 14:02:13 +0100

    Modify js file

--- Day_41_VerifyAccountUI/script.js
+++ Day_41_VerifyAccountUI/script.js
@@ -5,7 +5,10 @@ codes[0].focus();
 codes.forEach((code, i) => {
   code.addEventListener("keydown", (e) => {
     if (e.key >= 0 && e.key <= 9) {
+      codes[i].value = "";
       setTimeout(() => codes[i + 1].focus(), 10);
+    } else if (e.key === "Backspace") {
+      setTimeout(() => codes[i - 1].focus(), 10);
     }
   });
 });
